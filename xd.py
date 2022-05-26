3ef817974915755254e44324473ab428c0dd877f
Sachin P <66244820+IamGroot111@users.noreply.github.com>
2022-05-26 21:41:38 +0530

    Update xd.py

--- xd.py
+++ xd.py
@@ -1,44 +1,39 @@
 class student:
-    def __init__(self,name,subject):
+    def __init__(self,name):
         self.name=name
-        self.subject=subject
-    def getsub(self):
-        return self.subject
     def getname(self):
         return self.name        
-    def appendsub(self,sub):
-        self.subject.add(sub) 
-    def deets(self):
-        return self.name,self.subject
+    def appendsub(self,sub,dat):
+        if self.name not in dat:
+            dat[self.name]= set()
+        dat[self.name].add(sub)
 class subject:
     def __init__(self,name,code):
         self.subname=name        
         self.code=code
     def getsubname(self):
         return self.subname
+
+data=dict()
+
 English=subject("English","eng1012D")
 Maths=subject("Maths","ma2021D")
 Comp=subject("Computer Science","cs3120D")
-sac=student("Sachin",{English.getsubname()})
-cas=student("Cashin",{Maths.getsubname(),English.getsubname()})
-data=dict()
-data[sac.getname()]=sac.getsub()
-data[cas.getname()]=cas.getsub()
-print(sac.getname()) 
-print(sac.getsub())
-print(cas.deets())
-sac.appendsub(Comp.getsubname())
-print(sac.deets())
+
+sac=student("Sachin")
+sac.appendsub(English.getsubname(),data)
+
+cas=student("Cashin")
+cas.appendsub(Maths.getsubname(),data)
+
+sac.appendsub(Comp.getsubname(),data)
 print(data)
 
 def search(name):
-    c=0   
-    for names in data:
-        if (names==name):
-            print("The Subjects {} has enrolled in are:{}".format(name,data[name]))
-            c=1
-            break
-    if(c==0):
-        print("No student found with such a name")
+    if name not in data:
+        print("No student with such a name")
+        return
+    print(data[name])
 search("Sachin")        
 search("Aschin")
+            
