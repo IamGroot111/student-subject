31ab941cf8bb92ba9167d8b68b1d07318f0b119d
Sachin P <66244820+IamGroot111@users.noreply.github.com>
2022-05-26 20:57:54 +0530

    Update xd.py

--- xd.py
+++ xd.py
@@ -21,8 +21,24 @@ Maths=subject("Maths","ma2021D")
 Comp=subject("Computer Science","cs3120D")
 sac=student("Sachin",{English.getsubname()})
 cas=student("Cashin",{Maths.getsubname(),English.getsubname()})
+data=dict()
+data[sac.getname()]=sac.getsub()
+data[cas.getname()]=cas.getsub()
 print(sac.getname()) 
 print(sac.getsub())
 print(cas.deets())
 sac.appendsub(Comp.getsubname())
 print(sac.deets())
+print(data)
+
+def search(name):
+    c=0   
+    for names in data:
+        if (names==name):
+            print("The Subjects {} has enrolled in are:{}".format(name,data[name]))
+            c=1
+            break
+    if(c==0):
+        print("No student found with such a name")
+search("Sachin")        
+search("Aschin")
